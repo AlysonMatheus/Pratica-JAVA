fa1059d9ee4f4235276265b59beb9bfe80e4dc93
AlysonMatheus <alyson2000@live.com>
2024-12-06 18:21:02 -0300

    Strings metodos

--- src/javacore/String/teste/StringTest01.java
+++ src/javacore/String/teste/StringTest01.java
@@ -4,7 +4,7 @@ public class StringTest01 {
     public static void main(String[] args) {
         String nome = "Alyson ";//String  constant pool
         String nome2= "Alyson";
-       nome = nome.concat("Matheus");
+       nome = nome.concat("Matheus");// nome += "Matheus"
         System.out.println(nome);
         System.out.println(nome == nome2);
         String nome3 = new String("Alyson");// 1 variavel de referencia, 2 objeto do tipo string, 3 uma string no pool de string
