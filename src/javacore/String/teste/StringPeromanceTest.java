326782034e46a23cdc8fcfb6ec6296f9a1d37c05
AlysonMatheus <alyson2000@live.com>
2024-12-10 11:48:34 -0300

    Update StringPeromanceTest.java

--- src/javacore/String/teste/StringPeromanceTest.java
+++ src/javacore/String/teste/StringPeromanceTest.java
@@ -4,14 +4,43 @@ public class StringPeromanceTest {
     public static void main(String[] args) {
         long inicio = System.currentTimeMillis();
         concatSring(100000);
-        long fim =System.currentTimeMillis();
-        System.out.println("Tempo gasto para String " +(fim-inicio)+"ms");
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(100000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String " + (fim - inicio) + "ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(100000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String " + (fim - inicio) + "ms");
+
     }
-    private static void concatSring(int tamanho){
-        String texto =  "";
+
+    private static void concatSring(int tamanho) {
+        String texto = "";
         for (int i = 0; i < tamanho; i++) {
-            texto +=i;//0,01,012,013
-            
+            texto += i;//0,01,012,013
+
         }
     }
+
+    private static void concatStringBuilder(int tamanho) {
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+
+        }
+    }
+    private static void concatStringBuffer(int tamanho) {
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho; i++) {
+            sb.append(i);
+
+        }
+    }
+
+
 }
