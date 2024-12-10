09620432a59490330f007f352753fa4b1b9823e0
AlysonMatheus <alyson2000@live.com>
2024-12-10 12:16:42 -0300

    Update StringBuilderTest01.java

--- src/javacore/String/teste/StringBuilderTest01.java
+++ src/javacore/String/teste/StringBuilderTest01.java
@@ -4,9 +4,12 @@ public class StringBuilderTest01 {
     public static void main(String[] args) {
         String nome ="Alyson";
         nome.concat("Matheus");
+        nome.substring(0,2);
         System.out.println(nome);
         StringBuilder sb = new StringBuilder("Alyson");
-        sb.append(" Mathues");
+        sb.append(" Mathues").append(" Dev Java");
+        sb.reverse();
+        sb.reverse();
         System.out.println(sb);
     }
 }
