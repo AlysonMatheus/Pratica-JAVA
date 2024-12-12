de2945494c5a02a5f9f7542bae3fb87577643964
AlysonMatheus <alyson2000@live.com>
2024-12-12 11:46:54 -0300

    Create DataTest01.java

---
+++ src/javacore/Datas/test/DataTest01.java
@@ -0,0 +1,9 @@
+package javacore.Datas.test;
+
+import java.util.Date;
+
+public class DataTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();// Long representando o milesgundos 1000000
+    }
+}
