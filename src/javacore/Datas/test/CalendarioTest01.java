2df50ac551f7abc6ea0f6d86a451178a39155ecd
AlysonMatheus <alyson2000@live.com>
2024-12-12 12:04:50 -0300

    Update CalendarioTest01.java

--- src/javacore/Datas/test/CalendarioTest01.java
+++ src/javacore/Datas/test/CalendarioTest01.java
@@ -14,10 +14,13 @@ public class CalendarioTest01 {
         System.out.println(c.get(Calendar.DAY_OF_YEAR));
         System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
 
+        c.add(Calendar.DAY_OF_MONTH,5);
 
         Date date = c.getTime();
-
-
+        System.out.println(date);
         System.out.println(c);
+
+
+
     }
 }
