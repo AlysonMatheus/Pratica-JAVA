f4b3d3d126e2c0ab6bbdfb5babcfa087839c505f
AlysonMatheus <alyson2000@live.com>
2024-12-12 11:52:55 -0300

    Update DataTest01.java

--- src/javacore/Datas/test/DataTest01.java
+++ src/javacore/Datas/test/DataTest01.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 public class DataTest01 {
     public static void main(String[] args) {
-        Date date = new Date();// Long representando o milesgundos 1000000
+        Date date = new Date(1734015071168L);// Long representando o milesgundos 1000000
+        date.setTime(date.getTime()+3_600_000);
+        System.out.println(date);
     }
 }
