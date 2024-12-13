fe3e39bdb0ad67776d3d0e30cf0d72b6304969e7
AlysonMatheus <alyson2000@live.com>
2024-12-13 13:57:06 -0300

    Update LocaleTest01.java

--- src/javacore/Datas/test/LocaleTest01.java
+++ src/javacore/Datas/test/LocaleTest01.java
@@ -8,11 +8,13 @@ public class LocaleTest01 {
     public static void main(String[] args) {
         Locale localeItaly = new Locale("it", "IT");
         Locale localech = new Locale("it", "CH");
+        Locale localeIndia = new Locale("hi", "IN");
         Calendar calender = Calendar.getInstance();
         DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
         DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localech);
-
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL,localeIndia);
         System.out.println("Italia "+ df1.format(calender.getTime()));
         System.out.println("Suiça "+ df2.format(calender.getTime()));
+        System.out.println("India "+ df3.format(calender.getTime()));
     }
 }
