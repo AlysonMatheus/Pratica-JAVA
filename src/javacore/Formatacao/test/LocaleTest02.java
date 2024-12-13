d033289c3e03639f78cb0eb0437000167975620e
AlysonMatheus <alyson2000@live.com>
2024-12-13 14:16:31 -0300

    Update LocaleTest02.java

--- src/javacore/Formatacao/test/LocaleTest02.java
+++ src/javacore/Formatacao/test/LocaleTest02.java
@@ -1,7 +1,19 @@
 package javacore.Formatacao.test;
 
+import java.util.Locale;
+
 public class LocaleTest02 {
     public static void main(String[] args) {
-        
+        System.out.println(Locale.getDefault());
+        String[] isoCountries = Locale.getISOCountries();
+        String[] isoLangueges = Locale.getISOLanguages();
+
+        for (String isoLanguage : isoLangueges){
+            System.out.print(isoLanguage+" ");
+        }
+        for (String isoCountrie : isoCountries){
+            System.out.print(isoCountrie+" ");
+
+        }
     }
 }
