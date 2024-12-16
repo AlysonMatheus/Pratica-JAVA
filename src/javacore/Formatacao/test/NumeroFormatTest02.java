a6325e01576b5880272e597dfdb960020942ab6d
AlysonMatheus <alyson2000@live.com>
2024-12-15 23:28:56 -0300

    Create NumeroFormatTest02.java

---
+++ src/javacore/Formatacao/test/NumeroFormatTest02.java
@@ -0,0 +1,25 @@
+package javacore.Formatacao.test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumeroFormatTest02 {
+    public static void main(String[] args) {
+        Locale localeDefaullt = Locale.getDefault();
+        Locale localeBR= new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALIAN;
+
+        NumberFormat [] nfa = new NumberFormat[4];
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeBR);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+
+      double valor = 10_000.3130;
+      for (NumberFormat numberFormat :nfa){
+          System.out.println(numberFormat.format(valor));
+      }
+
+    }
+}
