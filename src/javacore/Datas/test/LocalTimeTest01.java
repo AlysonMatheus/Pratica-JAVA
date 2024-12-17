1400f86705e725ef6f3e65dd6bc26d9d145383ff
AlysonMatheus <alyson2000@live.com>
2024-12-17 11:28:52 -0300

    Create LocalTimeTest01.java

---
+++ src/javacore/Datas/test/LocalTimeTest01.java
@@ -0,0 +1,10 @@
+package javacore.Datas.test;
+
+import java.time.LocalTime;
+
+public class LocalTimeTest01 {
+    public static void main(String[] args) {
+      LocalTime time =  LocalTime.of(23,12,34);
+        System.out.println(time);
+    }
+}
