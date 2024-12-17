53eebb50c7286018249429c1747485e6ecdccb16
AlysonMatheus <alyson2000@live.com>
2024-12-17 11:15:47 -0300

    Novos comandos para mes, data e ano

--- src/javacore/Datas/test/LocalDateTest01.java
+++ src/javacore/Datas/test/LocalDateTest01.java
@@ -1,5 +1,8 @@
 package javacore.Datas.test;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -7,5 +10,13 @@ public class LocalDateTest01 {
     public static void main(String[] args) {
         System.out.println(new Date());
         System.out.println(Calendar.getInstance());
+        System.out.println(Month.DECEMBER.getValue());
+       LocalDate date = LocalDate.of(2024,Month.DECEMBER, 22);
+        System.out.println(date.getYear());
+        System.out.println(date.getMonth());
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.getDayOfWeek());
+        System.out.println(date.lengthOfMonth());
+        System.out.println(date.isLeapYear());
     }
 }
