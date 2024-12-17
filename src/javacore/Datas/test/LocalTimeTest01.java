144e05f3fdf3809a79089fb39559eecd0a18cc4b
AlysonMatheus <alyson2000@live.com>
2024-12-17 11:33:27 -0300

    Update LocalTimeTest01.java

--- src/javacore/Datas/test/LocalTimeTest01.java
+++ src/javacore/Datas/test/LocalTimeTest01.java
@@ -1,10 +1,19 @@
 package javacore.Datas.test;
 
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
 
 public class LocalTimeTest01 {
     public static void main(String[] args) {
       LocalTime time =  LocalTime.of(23,12,34);
-        System.out.println(time);
+      LocalTime timeNow = LocalTime.now();
+      System.out.println(time);
+        System.out.println(timeNow);
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.get(ChronoField.HOUR_OF_AMPM));
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
     }
 }
