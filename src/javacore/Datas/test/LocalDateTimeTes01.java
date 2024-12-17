0ee8e390874ba50c26bcd929fcc01b190a7192a8
AlysonMatheus <alyson2000@live.com>
2024-12-17 11:46:47 -0300

    Update LocalDateTimeTes01.java

--- src/javacore/Datas/test/LocalDateTimeTes01.java
+++ src/javacore/Datas/test/LocalDateTimeTes01.java
@@ -2,13 +2,24 @@ package javacore.Datas.test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 
 public class LocalDateTimeTes01 {
     public static void main(String[] args) {
         LocalDateTime localDateTime = LocalDateTime.now();
-        LocalDate date  = LocalDate.of(2024,10,1);
+
+        LocalDate date  = LocalDate.parse("2024-08-01");
+
+        LocalTime time= LocalTime.parse("09:45:22");
         System.out.println(localDateTime);
         System.out.println(date);
+        System.out.println(time);
+        LocalDateTime hora = date.atTime(time);
+        LocalDateTime hora2 =  time.atDate(date);
+        System.out.println(hora);
+        System.out.println(hora2);
+
     }
 
 }
