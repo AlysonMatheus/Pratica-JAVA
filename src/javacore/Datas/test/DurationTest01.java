00ffa94648089ece1e8e911e65282a566b36f6da
AlysonMatheus <alyson2000@live.com>
2024-12-17 15:24:56 -0300

    Update DurationTest01.java

--- src/javacore/Datas/test/DurationTest01.java
+++ src/javacore/Datas/test/DurationTest01.java
@@ -1,6 +1,7 @@
 package javacore.Datas.test;
 
 import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class DurationTest01 {
     public static void main(String[] args) {
@@ -11,11 +12,15 @@ public class DurationTest01 {
         Duration duration = Duration.between(now,nowAfterTwoYears);
         Duration duration1 = Duration.between(timenow, timeMinus7Hours);
         Duration duration2 = Duration.between(Instant.now(),Instant.now().plusSeconds(1000));
-         Duration duration3 = Duration.ofDays(20);
+        Duration duration3 = Duration.ofDays(20);
+        Duration duration4 = Duration.ofMinutes(20);
+        Duration duration5 = Duration.of(20, ChronoUnit.HOURS);
         System.out.println(duration);
         System.out.println(duration1);
         System.out.println(duration2);
         System.out.println(duration3);
+        System.out.println(duration4);
+        System.out.println(duration5);
 
 
     }
