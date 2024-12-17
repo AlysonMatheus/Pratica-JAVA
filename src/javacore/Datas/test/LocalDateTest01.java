b432c62aa991e94b26d20a431777c00dd9af9bd0
AlysonMatheus <alyson2000@live.com>
2024-12-17 11:20:49 -0300

    Update LocalDateTest01.java

--- src/javacore/Datas/test/LocalDateTest01.java
+++ src/javacore/Datas/test/LocalDateTest01.java
@@ -3,8 +3,10 @@ package javacore.Datas.test;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
+import java.time.temporal.ChronoField;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class LocalDateTest01 {
     public static void main(String[] args) {
@@ -12,11 +14,17 @@ public class LocalDateTest01 {
         System.out.println(Calendar.getInstance());
         System.out.println(Month.DECEMBER.getValue());
        LocalDate date = LocalDate.of(2024,Month.DECEMBER, 22);
-        System.out.println(date.getYear());
+        LocalDate agora = LocalDate.now();
+       System.out.println(date.getYear());
         System.out.println(date.getMonth());
         System.out.println(date.getDayOfMonth());
         System.out.println(date.getDayOfWeek());
         System.out.println(date.lengthOfMonth());
         System.out.println(date.isLeapYear());
+        System.out.println(date.get(ChronoField.YEAR));
+        System.out.println(date);
+        System.out.println(agora);
+        System.out.println(LocalDate.MAX);
+        System.out.println(LocalDate.MIN);
     }
 }
