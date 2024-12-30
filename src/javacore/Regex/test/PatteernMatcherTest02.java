07f08bb357ba6d8adbf503317326b9b61b05521e
AlysonMatheus <alyson2000@live.com>
2024-12-30 20:12:39 -0300

    Create PatteernMatcherTest02.java

---
+++ src/javacore/Regex/test/PatteernMatcherTest02.java
@@ -0,0 +1,53 @@
+package javacore.Regex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatteernMatcherTest02 {
+    public static void main(String[] args) {
+        // \d = Todos os digitos
+        // /D = Tudo o que não for digito
+        //\s - Espaçoes em branco \t \n \f \f \r
+        // \S = Todos os caracters excluindo os brancos
+        // \W = a-ZA-Z, digitos, _
+        // \W =Tudo que não for incclluso no \w
+        String regex = "\\d";
+        String regex1 ="\\D";
+        String regex2 ="\\s";
+        String regex3 ="\\S";
+
+        //String texto =  "abaaba";
+        String texto2 =  "@aaa212&  #1910";
+        Pattern pattern = Pattern.compile(regex);
+        Pattern pattern1 = Pattern.compile(regex1);
+        Pattern pattern2 = Pattern.compile(regex2);
+
+        Matcher matcher = pattern.matcher(texto2);
+        Matcher matcher1 = pattern1.matcher(texto2);
+        Matcher matcher2 = pattern2.matcher(texto2);
+
+        System.out.println("Texto  "+texto2);
+        System.out.println("indice: 123456789");
+        System.out.println("regex " +regex);
+        System.out.println("regex1 " +regex1);
+        System.out.println("regex2 " +regex2);
+        System.out.println("Posições encontradas");
+
+        while (matcher.find()){
+            System.out.print(matcher.start()+" "+matcher.group()+"\n");
+        }
+        System.out.println("----------------------------");
+        while (matcher1.find()){
+            System.out.println(matcher1.start()+" "+matcher1.group()+"\n");
+        }
+        System.out.println("----------------------------");
+        while (matcher2.find()){
+            System.out.println(matcher2.start()+" "+matcher2.group()+"\n");
+        }
+
+
+
+
+
+    }
+}
