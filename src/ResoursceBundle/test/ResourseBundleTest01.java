c9a39883b4ae1f489c0fb66e5ec8c936c2da5a45
AlysonMatheus <alyson2000@live.com>
2024-12-30 19:23:37 -0300

    Novas bases em outros idiomas

--- src/ResoursceBundle/test/ResourseBundleTest01.java
+++ src/ResoursceBundle/test/ResourseBundleTest01.java
@@ -6,7 +6,11 @@ import java.util.ResourceBundle;
 public class ResourseBundleTest01 {
     public static void main(String[] args) {
         System.out.println(Locale.getDefault());
-       ResourceBundle bundle = ResourceBundle.getBundle("mensagem", new Locale ("pt", "BR"));
+        ResourceBundle bundle = ResourceBundle.getBundle("mensagem", new Locale("pt", "BR"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+        bundle = ResourceBundle.getBundle("mensagem", new Locale("en", "US"));
         System.out.println(bundle.getString("hello"));
         System.out.println(bundle.getString("good.morning"));
 
