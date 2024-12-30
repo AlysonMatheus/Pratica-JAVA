b4fc18ef6408ebe04a1efc1812327d7cd4d0fc5f
AlysonMatheus <alyson2000@live.com>
2024-12-30 19:10:26 -0300

    Praticando ResourceBundle

---
+++ src/ResoursceBundle/test/ResourseBundleTest01.java
@@ -0,0 +1,14 @@
+package ResoursceBundle.test;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourseBundleTest01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+       ResourceBundle bundle = ResourceBundle.getBundle("mensagem", new Locale ("pt", "BR"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+    }
+}
