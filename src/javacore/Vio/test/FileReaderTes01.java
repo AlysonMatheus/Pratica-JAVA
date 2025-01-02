799f0751a8e859743a54094e31c9a91f71c0486e
AlysonMatheus <alyson2000@live.com>
2025-01-02 18:29:49 -0300

    Update FileReaderTes01.java

--- src/javacore/Vio/test/FileReaderTes01.java
+++ src/javacore/Vio/test/FileReaderTes01.java
@@ -1,4 +1,28 @@
 package javacore.Vio.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileReaderTes01 {
+    public static void main(String[] args) {
+        File  file = new File("file.txt");
+        try {
+            FileReader fr = new FileReader(file);
+
+         //   char [] in = new char [65];
+           // fr.read(in);
+            //for (char c : in){
+              //  System.out.println(c);
+            //}
+            int i;
+            while ((i = fr.read())!= -1){
+                System.out.print((char)i);
+            }
+        } catch (IOException e) {
+         e.printStackTrace();
+        }
+
+    }
 }
