2d88363ecbfc5a0b8380a64fc80cf99439e0c8bf
AlysonMatheus <alyson2000@live.com>
2025-01-08 22:44:42 -0300

    Create SimpleFileVisitorTest01.java

---
+++ src/javacore/Nio/SimpleFileVisitorTest01.java
@@ -0,0 +1,15 @@
+package javacore.Nio;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+class ListAllFiles
+
+public class SimpleFileVisitorTest01 {
+    public static void main(String[] args) {
+        Path root = Paths.get("");
+        Files.walkFileTree(root,);
+    }
+}
