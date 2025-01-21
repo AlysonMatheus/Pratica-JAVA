e8a9edd74808234ce1307c47b6420de003461b98
AlysonMatheus <alyson2000@live.com>
2025-01-21 20:06:51 -0300

    Aplicando BinarySearch

---
+++ src/javacore/Colecoes/test/BinarySeracjTest01.java
@@ -0,0 +1,23 @@
+package javacore.Colecoes.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinarySeracjTest01 {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList();
+        numeros.add(2);
+        numeros.add(0);
+        numeros.add(4);
+        numeros.add(3);
+        //(-(ponto de inserção) -1)
+        Collections.sort(numeros);
+        /// index 0,1,2,3
+        //value 0,2,3,4
+
+        System.out.println(Collections.binarySearch(numeros,0));
+        System.out.println(Collections.binarySearch(numeros,-1));
+
+    }
+}
---
+++ src/javacore/Colecoes/test/BinarySeracjTest02.java
@@ -0,0 +1,30 @@
+package javacore.Colecoes.test;
+
+import javacore.Colecoes.dominio.Manga;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinarySeracjTest02 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        MangaByIdComparator mangaByIdComparator = new MangaByIdComparator();
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9));
+        mangas.add(new Manga(2L, "Attack on titan", 20.0));
+        mangas.add(new Manga(1L, "Berserk", 13.0));
+        mangas.add(new Manga(4L, "Pokemom", 4.0));
+        mangas.add(new Manga(3L, "Dragon ball Z", 15.0));
+        //Collections.sort(mangas);
+        mangas.sort(mangaByIdComparator);
+        for (Manga manga : mangas){
+            System.out.println(manga);
+        }
+        Manga mangasToSearch = new Manga(6L, "Dragon ball Z", 15.0);
+        System.out.println(Collections.binarySearch(mangas,mangasToSearch, mangaByIdComparator));
+
+
+
+
+    }
+}
