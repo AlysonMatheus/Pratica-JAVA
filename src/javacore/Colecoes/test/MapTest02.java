5b721fe8bf183dd998eb645462e8c2ac1900e1b4
AlysonMatheus <alyson2000@live.com>
2025-01-23 12:49:00 -0300

    Continuação Map

---
+++ src/javacore/Colecoes/test/MapTest02.java
@@ -0,0 +1,9 @@
+package javacore.Colecoes.test;
+
+import javacore.Colecoes.dominio.Consumidor;
+
+public class MapTest02 {
+    public static void main(String[] args) {
+        Consumidor consumidor1 = new Consumidor("Alyson Matheus");
+    }
+}
