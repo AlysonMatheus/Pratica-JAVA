becc869673876a00836a1aaea69afee0967f0fd9
AlysonMatheus <alyson2000@live.com>
2025-01-30 22:36:06 -0300

    Lambdas

--- src/javacore/Comportamento/Interfaces/CarPredicate.java
+++ src/javacore/Comportamento/Interfaces/CarPredicate.java
@@ -1,9 +1,12 @@
 package javacore.Comportamento.Interfaces;
 
 import javacore.Comportamento.dominio.Car;
-
+@FunctionalInterface
 public interface CarPredicate {
     boolean test(Car car);
+    /// anonimas, funções, conciso
+    //(parametro)-> <expressão>
+    //(Car car) ->car.getColor(.equals("green");
 
 
 }
