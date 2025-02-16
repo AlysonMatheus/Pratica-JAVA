2560ca5b042424c53a5056576bd4417bab137806
AlysonMatheus <alyson2000@live.com>
2025-02-16 14:23:49 -0300

    Sincronismo de thread

--- src/javacore/Threads/test/ThreadAccountTest01.java
+++ src/javacore/Threads/test/ThreadAccountTest01.java
@@ -3,7 +3,7 @@ package javacore.Threads.test;
 import javacore.Threads.dominio.Account;
 
 public class ThreadAccountTest01 implements Runnable {
-    private Account accuont = new Account();
+    private final Account accuont = new Account();
 
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
@@ -20,19 +20,35 @@ public class ThreadAccountTest01 implements Runnable {
             if (accuont.getBalance() < 0) {
                 System.out.println("LASCOU");
             }
+
         }
 
 
     }
+    private static void pprint(){
+        synchronized (ThreadAccountTest01.class){
+            
+        }
+
+    }
 
 
-    private void withdrawal(int amount) {
-        if (accuont.getBalance() >= amount) {
-            System.out.println(getThreadName() + " esta indo sacar dinheiro");
-            accuont.withdrawal(amount);
-            System.out.println(getThreadName() + " completou o saque, valor atual da  conta " + accuont.getBalance());
-        } else {
-            System.out.println("Sem dinherio " + getThreadName() + " efetuar o saque " + accuont.getBalance());
+    private  void withdrawal(int amount) {
+
+        synchronized (accuont) {
+            System.out.println(getThreadName() + " dentro do synchornized ");
+            if (accuont.getBalance() >= amount) {
+                System.out.println(getThreadName() + " esta indo sacar dinheiro");
+                accuont.withdrawal(amount);
+                System.out.println(getThreadName() + " completou o saque, valor atual da  conta " + accuont.getBalance());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                System.out.println("Sem dinherio " + getThreadName() + " efetuar o saque " + accuont.getBalance());
+            }
         }
     }
 
