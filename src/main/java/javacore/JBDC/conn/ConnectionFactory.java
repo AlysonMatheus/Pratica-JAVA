53413700e2d0e362080ff8093a24dca520c4640d
AlysonMatheus <alyson2000@live.com>
2025-03-19 20:10:08 -0300

    Instalando e adicionando maven ao projeto

---
+++ src/main/java/javacore/JBDC/conn/ConnectionFactory.java
@@ -0,0 +1,10 @@
+package javacore.JBDC.conn;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+//java.sql = Connection , Statement, ResultSet ,DriveManager
+public static void main(String[] args) {
+
+}
+}
