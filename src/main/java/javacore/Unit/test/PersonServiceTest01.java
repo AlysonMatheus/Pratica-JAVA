f8dab3672ad6518afe6c4f64ac41448b14df88e3
AlysonMatheus <alyson2000@live.com>
2025-04-12 16:32:50 -0300

    Testes unitários com jUnit

---
+++ src/main/java/javacore/Unit/test/PersonServiceTest01.java
@@ -0,0 +1,16 @@
+package javacore.Unit.test;
+
+import javacore.Unit.dominio.Person;
+import javacore.Unit.service.PersonService;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class PersonServiceTest01 {
+    public static void main(String[] args) {
+        Person person = new Person(15);
+        PersonService personService = new PersonService();
+        log.info("Is Adult '{}'",personService.isAdult(person));
+
+
+    }
+}
